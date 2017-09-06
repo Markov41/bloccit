641564a40f3a4982755a7beb6150389e09226bd9
Kevin Lattner <analogy48@yahoo.com>
2017-09-06 19:35:02 +0000

    Assignment 23 - Rails: Authentication - User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase if email.present? }
+    before_save :format_name
     
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
     
@@ -12,5 +13,15 @@ class User < ApplicationRecord
         length: { minimum: 3, maximum: 254 }
     
     has_secure_password
+    
+    before_save { self.name = name.split(" ") }
+    
+    def format_name
+        name_array = []
+        self.name.split.each do |n|
+            name_array << n.capitalize
+        end
+        self.name = name_array.join(" ")
+    end
     
 end
