69f5f6c69e81d6916f382ff9f8cd1ae2c6824106
Kevin Lattner <analogy48@yahoo.com>
2017-09-16 14:30:16 +0000

    Checkpoint 25 - Rails: Authentication - Signing In

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,6 +13,7 @@ module Workspace
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << File.join(config.root, "lib")
+    config.action_controller.permit_all_parameters = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
