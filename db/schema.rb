a61e47dc467e8336481f92aedd40435aba6464be
Kevin Lattner <analogy48@yahoo.com>
2017-09-25 20:56:12 +0000

    Checkpoint 27 - Rails: Authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919175807) do
+ActiveRecord::Schema.define(version: 20170922180639) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20170919175807) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
