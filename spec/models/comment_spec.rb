527131d7a46b098478712e550f0056d0dfadf413
Kevin Lattner <analogy48@yahoo.com>
2017-10-04 22:47:59 +0000

    checkpoint 31 - Rails: Public Profiles

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:user) { create(:user) }
+  let(:topic) { create(:topic) }
   let(:comment) {Comment.create!(body: "Comment Body", post: post, user: user) }
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:post) { create(:post) }
   
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
